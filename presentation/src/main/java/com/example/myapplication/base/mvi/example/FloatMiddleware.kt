1a1009c7a2c716f89cd585c1caf6d5668260ace9
Vyacheslav Vorozheykin <qwe2rty1@gmail.com>
2020-03-17 00:17:22 +0700

    Delete unused.

--- presentation/src/main/java/com/example/myapplication/base/mvi/example/FloatMiddleware.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication.base.mvi.example
-
-import com.example.myapplication.base.mvi.Middleware
-import io.reactivex.Observable
-
-class FloatMiddleware : Middleware<Float, String> {
-	override fun apply(events: Observable<Float>) = Observable.just("a")
-}
